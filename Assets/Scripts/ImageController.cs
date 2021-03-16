b5398d8e0ca4c98344072a537b7a6d6e25924df7
Joseph-Cha <joseph.c@kakao.com>
2021-03-17 01:24:20 +0900

    이미지 요청까지 완료

--- Assets/Scripts/ImageController.cs
+++ Assets/Scripts/ImageController.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+ using Newtonsoft.Json;
+
 
 public class ImageController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private InputField InputField;
+
+    private string _imageName
     {
+        get
+        {
+            return InputField.text; 
+        }
+    }
+
+    private string _serverURL
+    {
+        get
+        {
+            return "https://search.naver.com/search.naver?where=image&sm=tab_jum&query=";
+        }
+    }
+
+    public void OnRequest()
+    {
+        StartCoroutine(RequestImage());
+    }
+
+    private IEnumerator RequestImage()
+    {
+        string requestURL = _serverURL + _imageName;  
+
+        using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(requestURL))
+        {
+            if(!request.isNetworkError && !request.isHttpError)
+            {
+                yield return request.SendWebRequest();
+                DownloadImage(request);
+            }
+        }
+    }
+
+    private void DownloadImage(UnityWebRequest request)
+    {
+        Texture2D texture = DownloadHandlerTexture.GetContent(request);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
 }
