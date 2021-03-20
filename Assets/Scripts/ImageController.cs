0d31b66078d877c1c388b2762acd54827cdd3a62
Joseph-Cha <joseph.c@kakao.com>
2021-03-20 18:12:38 +0900

    이미지 사이트에 접근해서 이미지 텍스처 가지고 오기 성공

--- Assets/Scripts/ImageController.cs
+++ Assets/Scripts/ImageController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
  using Newtonsoft.Json;
-
+using System;
 
 public class ImageController : MonoBehaviour
 {
     [SerializeField]
-    private InputField InputField;
+    private InputField InputField = null;
 
+    [SerializeField]
+    private Image image = null;
     private string _imageName
     {
         get
@@ -19,40 +21,91 @@ public class ImageController : MonoBehaviour
         }
     }
 
-    private string _serverURL
-    {
-        get
-        {
-            return "https://search.naver.com/search.naver?where=image&sm=tab_jum&query=";
-        }
-    }
+    // private string _serverURL
+    // {
+    //     get
+    //     {
+    //         return "https://www.google.com/search?q=%EA%B0%95%EC%9D%B4%EC%A7%80&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiXifTovL7vAhWEHHAKHTIDBCIQ_AUoAXoECAQQAw&biw=1792&bih=975";
+    //     }
+    // }
+    // private string _serverURL2
+    // {
+    //     get
+    //     {
+    //         return "&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiXifTovL7vAhWEHHAKHTIDBCIQ_AUoAXoECAQQAw&biw=1792&bih=975";
+    //     }
+    // }
+    
+    // private void Start()
+    // {   
+        
+    //    // new ImageObject(image);
+    // }
 
     public void OnRequest()
     {
         StartCoroutine(RequestImage());
     }
 
+    
     private IEnumerator RequestImage()
     {
-        string requestURL = _serverURL + _imageName;  
+        // string requestURL = _serverURL + _imageName + _serverURL2;  
+        // using (UnityWebRequest request = UnityWebRequest.Get(requestURL))
+        // {
+        //     if(!request.isNetworkError && !request.isHttpError)
+        //     {
+        //         yield return request.SendWebRequest();
+        //         string[] pages = requestURL.Split('/');
+        //         int page = pages.Length - 1;
+        //         if(request.isDone)
+        //         {
+        //             Debug.Log(pages[page] + ":\nReceived: " + request.downloadHandler.text);                    
+        //         }
+        //     }
+        // }
+        
+        string requestURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi9kL1x8wgF0xKfd3dHbSHYCrZ-uMDuCxsyL5ss6UdXMM2UvzBk9A7J2EF4Uo&amp;s";
 
         using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(requestURL))
         {
             if(!request.isNetworkError && !request.isHttpError)
             {
                 yield return request.SendWebRequest();
-                DownloadImage(request);
+                if(request.isDone)
+                {
+                    Debug.Log($"request.result : {request.result}");
+                    DownloadImage(request);
+                }
             }
         }
     }
 
     private void DownloadImage(UnityWebRequest request)
     {
-        Texture2D texture = DownloadHandlerTexture.GetContent(request);
-        
+        // 이미지 다운로드 
+        Texture2D texture = null;
+        try
+        {
+            texture = DownloadHandlerTexture.GetContent(request);
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+
+        // 다운로드한 이미지를 토대로 이미지 Sprite 생성
+        Sprite sprite = null;
+        try
+        {
+            sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));   
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+        image.sprite = sprite;
+        image.SetNativeSize();
     }
-
-
-
-
 }
