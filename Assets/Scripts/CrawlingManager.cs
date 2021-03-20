6c541d32fc422227e8b07a86f39b7ad18138e2d9
Joseph-Cha <joseph.c@kakao.com>
2021-03-21 00:17:48 +0900

    수정된 설계대로 클래스 인터페이스 작성 및 일부 구현

---
+++ Assets/Scripts/CrawlingManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DataTpye
+{
+    Image
+}
+public class CrawlingManager : MonoBehaviour
+{
+    public void CrawlingDate(string keyword, DataTpye dataType)
+    {
+        switch (dataType)
+        {
+            case DataTpye.Image:
+                CrawlingImage(keyword);
+                break;
+            default:
+                Debug.Log("There is no DataTpye. You can only Crawl Date which is Image");
+                break;
+        }
+    }
+
+    private void CrawlingImage(string keyword)
+    {
+
+    }
+    private void SaveSrc()
+    {
+
+    }
+    private void SaveSprite()
+    {
+
+    }
+
+}
