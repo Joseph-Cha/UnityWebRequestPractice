a4b3a8354767ea02b67f0075b3c5bf2f2a0138d1
dhcha-virnect <75019048+Joseph-Cha@users.noreply.github.com>
2021-12-15 10:11:55 +0900

    폴더 정리

--- Assets/Script/Stock/StockRequest.cs
+++ Assets/Script/Stock/StockRequest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrawlingData : MonoBehaviour
+public class StockRequest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
